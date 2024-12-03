4942d03b8454ed948810e2f6e4b7cf291c0e2666
Eshwar-MS <mseswar2002@gmail.com>
2024-12-03 20:31:40 +0530

    First Custom Component

---
+++ src/Components/Post.jsx
@@ -0,0 +1,12 @@
+//capital names for files containing React components
+
+function Post() {
+  return (
+    <div>
+      <p>Eshwar</p>
+      <p>I like working on React!!!</p>
+    </div>
+  );
+}
+
+export default Post;
