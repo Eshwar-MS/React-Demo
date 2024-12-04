964bdf68ca708af085c4936991e792ced44b3993
Eshwar-MS <mseswar2002@gmail.com>
2024-12-04 21:28:56 +0530

    Lifting State up

--- src/Components/NewPost.jsx
+++ src/Components/NewPost.jsx
@@ -1,28 +1,16 @@
 import { useState } from "react";
 import classes from "./NewPost.module.css";
 
-function NewPost() {
-  const [updatedText, setUpdatedText] = useState("");
-  //The useState() hook not only stores a value but also tells React to re-render the component whenever the state is updated.
-  // updatedText: A state variable that holds the current value of the textarea.
-  // setUpdatedText: A function provided by useState to update the value of updatedText.
-  // Initial Value: The state is initialized with an empty string ("").
-
-  function changeBodyHandler(event) {
-    setUpdatedText(event.target.value);
-    //setUpdatedText(event.target.value) updates the state with the new value, triggering a re-render.
-  }
-
+function NewPost(props) {
   return (
     <form className={classes.form}>
       <p>
-        <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} onChange={changeBodyHandler} />
-      </p>
-      <p>{updatedText}</p>
-      <p>
         <label htmlFor="name">Your name</label>
-        <input type="text" id="name" required />
+        <input type="text" id="name" required onChange={props.onAuthorChange} />
+      </p>
+      <p>
+        <label htmlFor="body">Text</label>
+        <textarea id="body" required rows={3} onChange={props.onBodyChange} />
       </p>
     </form>
   );
--- src/Components/PostsList.jsx
+++ src/Components/PostsList.jsx
@@ -1,13 +1,31 @@
 import NewPost from "./NewPost";
 import Post from "./Post";
 import styles from "./PostsList.module.css";
+import { useState } from "react";
 
 function PostsList() {
+  const [updatedText, setUpdatedText] = useState("");
+  const [updatedAuthor, setUpdatedAuthor] = useState("");
+  //The useState() hook not only stores a value but also tells React to re-render the component whenever the state is updated. //any chsnges in these two states the whole (PostsList ) component will be rendered againn
+  // updatedText: A state variable that holds the current value of the textarea.
+  // setUpdatedText: A function provided by useState to update the value of updatedText.
+  // Initial Value: The state is initialized with an empty string ("").
+
+  function bodyChangeHandler(event) {
+    setUpdatedText(event.target.value);
+    //setUpdatedText(event.target.value) updates the state with the new value, triggering a re-render.
+  }
+  function authorChangeHandler(event) {
+    setUpdatedAuthor(event.target.value);
+  }
   return (
     <>
-      <NewPost />
+      <NewPost
+        onBodyChange={bodyChangeHandler}
+        onAuthorChange={authorChangeHandler}
+      />
       <ul className={styles.posts}>
-        <Post author="Eshwar" message="I am Coding with React !!" />
+        <Post author={updatedAuthor} message={updatedText} />
         <Post author="Ashish" message="React is very convenient to use !!" />
       </ul>
     </>
