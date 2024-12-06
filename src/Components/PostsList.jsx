da6c7d83fbbca5d709a5d48bcd15247bfa78230d
Eshwar-MS <mseswar2002@gmail.com>
2024-12-06 19:02:32 +0530

    Sending a POST HTTP Request

--- src/Components/PostsList.jsx
+++ src/Components/PostsList.jsx
@@ -8,6 +8,13 @@ function PostsList(props) {
   const [posts, setPosts] = useState([]);
 
   function addPostsHandler(postData) {
+    fetch("http://localhost:8080/posts", {
+      method: "POST",
+      body: JSON.stringify(postData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
     setPosts((existingData) => [postData, ...existingData]);
   }
 
@@ -20,11 +27,19 @@ function PostsList(props) {
       ) : (
         false
       )}
-      <ul className={styles.posts}>
-        {posts.map((post, index) => (
-          <Post key={index} author={post.name} body={post.body} />
-        ))}
-      </ul>
+      {posts.length > 0 && (
+        <ul className={styles.posts}>
+          {posts.map((post, index) => (
+            <Post key={index} author={post.name} body={post.body} />
+          ))}
+        </ul>
+      )}
+      {posts.length === 0 && (
+        <div style={{ textAlign: "center", color: "white" }}>
+          <h2>There are no posts yet.</h2>
+          <p>Start adding some !!!</p>
+        </div>
+      )}
     </>
   );
 }
