080b4e0e69073ee17b962c5a9dd2625e28f68852
Eshwar-MS <mseswar2002@gmail.com>
2024-12-25 23:41:40 +0530

    Submitting Data with action()s [half]

--- src/Routes/NewPost.jsx
+++ src/Routes/NewPost.jsx
@@ -1,48 +1,23 @@
-import { useState } from "react";
 import classes from "./NewPost.module.css";
 import Modal from "../Components/Modal";
 import { Link } from "react-router-dom";
 
-function NewPost(props) {
-  const [updatedText, setUpdatedText] = useState("");
-  const [updatedAuthor, setUpdatedAuthor] = useState("");
+function NewPost() {
   //The useState() hook not only stores a value but also tells React to re-render the component whenever the state is updated. //any chsnges in these two states the whole (PostsList ) component will be rendered againn
   // updatedText: A state variable that holds the current value of the textarea.
   // setUpdatedText: A function provided by useState to update the value of updatedText.
   // Initial Value: The state is initialized with an empty string ("").
 
-  function bodyChangeHandler(event) {
-    setUpdatedText(event.target.value);
-    //setUpdatedText(event.target.value) updates the state with the new value, triggering a re-render.
-  }
-  function authorChangeHandler(event) {
-    setUpdatedAuthor(event.target.value);
-  }
-
-  function submitHandler(event) {
-    event.preventDefault();
-    const postData = {
-      name: updatedAuthor,
-      body: updatedText,
-    };
-    props.onAddPost(postData);
-    props.onCancel();
-  }
   return (
     <Modal>
-      <form className={classes.form} onSubmit={submitHandler}>
+      <form className={classes.form}>
         <p>
           <label htmlFor="name">Your name</label>
-          <input
-            type="text"
-            id="name"
-            required
-            onChange={authorChangeHandler}
-          />
+          <input type="text" id="name" name="author" required />
         </p>
         <p>
           <label htmlFor="body">Text</label>
-          <textarea id="body" required rows={3} onChange={bodyChangeHandler} />
+          <textarea id="body" name="body" required rows={3} />
         </p>
         <p className={classes.actions}>
           <Link to=".." type="button">
@@ -56,3 +31,13 @@ function NewPost(props) {
 }
 // .. = going up one level , here it is back to homepage  // similar to '/'
 export default NewPost;
+
+export function action() {
+  fetch("http://localhost:8080/posts", {
+    method: "POST",
+    body: JSON.stringify(postData),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+}
